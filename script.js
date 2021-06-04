ae817f2a1c39af442424e4f2e885d544f73e3928
Jaden Arceneaux <arceneauxjaden@gmail.com>
2021-06-04 09:25:00 -0700

    new scrolling mechanism

---
+++ script.js
@@ -0,0 +1,67 @@
+const body = document.getElementById("body");
+const arrow = document.getElementById("arrow");
+const main = document.getElementById("main");
+
+const pages = [
+  "intro-section",
+  "about-page",
+  "skills-page",
+  "projects-page",
+  "contact-page",
+];
+
+// find the current page the user is on
+let currentPage = window.location.href.split("#")[1];
+// handle if user is on root page
+if (!currentPage) currentPage = pages[0];
+
+function showArrow() {
+  // find the current page the user is on
+  let currentPage = window.location.href.split("#")[1];
+  // handle if user is on root page
+  if (!currentPage) currentPage = pages[0];
+  if (currentPage == pages[0]) {
+    arrow.style.opacity = "0";
+  } else {
+    arrow.style.opacity = "1";
+  }
+  console.log(currentPage, " checking");
+}
+
+showArrow();
+
+function adjPage(mod) {
+  let index = pages.indexOf(currentPage);
+  if (index + mod < 0) return;
+  if (index + mod >= pages.length) {
+    currentPage = pages[0];
+    window.location = "#" + pages[0];
+  } else {
+    currentPage = pages[index + mod];
+    window.location = "#" + pages[index + mod];
+  }
+}
+
+document.onkeydown = (key) => {
+  switch (key.code) {
+    case "ArrowUp":
+      key.preventDefault();
+      adjPage(-1);
+      break;
+    case "ArrowDown":
+      key.preventDefault();
+      adjPage(1);
+      break;
+    default:
+      console.log(key.code);
+  }
+};
+
+main.onscroll = (e) => {
+  showArrow();
+};
+
+arrow.addEventListener("click", (e) => {
+  e.preventDefault();
+  adjPage(1);
+});
