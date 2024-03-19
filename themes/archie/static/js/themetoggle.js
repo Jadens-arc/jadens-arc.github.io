91ab7f4fd94744ac23d42dea8343500ad47346b9
Jaden Arceneaux <arceneauxjaden@gmail.com>
2024-03-18 20:06:12 -0700

    notch color when toggling dark and light mode

--- themes/archie/static/js/themetoggle.js
+++ themes/archie/static/js/themetoggle.js
@@ -3,10 +3,12 @@ function setTheme(mode) {
     if (mode === "dark") {
         document.getElementById("darkModeStyle").disabled=false;
         document.getElementById("dark-mode-toggle").innerHTML = "<i data-feather=\"sun\"></i>";
+        document.querySelector('meta[name="theme-color"]')?.setAttribute("content", "#202124")
         feather.replace()
     } else if (mode === "light") {
         document.getElementById("darkModeStyle").disabled=true;
         document.getElementById("dark-mode-toggle").innerHTML = "<i data-feather=\"moon\"></i>";
+        document.querySelector('meta[name="theme-color"]')?.setAttribute("content", "white")
         feather.replace()
     }
 }
